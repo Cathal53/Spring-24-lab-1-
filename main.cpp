3b458577688c5125fbb0cc0e3dfe1ac03549c05f
Cathal53 <cathal53@hotmail.com>
2024-05-19 02:20:20 +0000

    After many build errors and commits I got the build to Succeeded ,
    Signed-off-by: Cathal53 <cathal53@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include "mbed.h"
 #include <cstdio>
 
-InterruptIn (buttnPC_13) ;
+InterruptIn buttn(PC_13);// this is the input pc13 the blue button
 
+volatile int myflag = 0; // Here I had to add the volatile function in front of the int as i was having a build error
+//I Created an integer variable and called it my flag
 void buttn_fall(){
-    printf("button pressed!!\r\n");
+    myflag = 1;
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
-    printf("This is mbed os v%d \r\n", MBED_VERSION")
-
+    printf("This is mbed os v%d \r\n", MBED_VERSION);// Here i had to print the statement 
     buttn.fall(&buttn_fall);
     while (true) {
-
-    }
-}
-
+        if(myflag == 1){
+            printf("button pressed!!\r\n");// Here i had to print the statement 
+            myflag = 0;// Had to reset my flag
+        }
+    } // After many build errors and commits I got the build to Succeeded
+}// I Improved the code performed a commit and published the code
