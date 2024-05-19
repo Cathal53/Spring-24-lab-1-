fc974950bc50014a9dd8a69260944ff084735b04
Cathal53 <cathal53@hotmail.com>
2024-05-19 01:39:59 +0000

    Lab 1  mbed OS Error - Print f statement error
    Signed-off-by: Cathal53 <cathal53@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "mbed.h"
+#include <cstdio>
+
+InterruptIn (buttnPC_13) ;
+
+void buttn_fall(){
+    printf("button pressed!!\r\n");
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
+    printf("This is mbed os v%d \r\n", MBED_VERSION")
+
+    buttn.fall(&buttn_fall);
     while (true) {
 
     }
